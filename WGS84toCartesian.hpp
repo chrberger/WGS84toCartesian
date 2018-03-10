fd30dea38f4cf7a7395e1f79aefedb8a59e74a93
Christian Berger <christian.berger@gu.se>
2018-03-10 17:37:59 +0100

    * Windows build fix
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- WGS84toCartesian.hpp
+++ WGS84toCartesian.hpp
@@ -25,10 +25,7 @@
 #ifndef WGS84TOCARTESIAN_HPP
 #define WGS84TOCARTESIAN_HPP
 
-#ifdef WIN32
-    #define _USE_MATH_DEFINES
-#endif
-#include <cmath>
+#include <math.h>
 
 #include <array>
 #include <limits>
